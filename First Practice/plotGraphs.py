24a765c23e6232968a5b52374887380ed39d6eef
= <=>
2023-04-20 12:29:02 -0500

    Update

--- First Practice/plotGraphs.py
+++ First Practice/plotGraphs.py
@@ -6,7 +6,7 @@ with open("sequentialOutput.txt", "r") as f:
       row = line.split(" ")
       sequentialResults[row[0]] = float(row[1])
 
-print(sequentialResults)
+#print(sequentialResults)
 
 ompResults = {}
 with open("OmpOutput.txt", "r") as f:
@@ -17,5 +17,34 @@ with open("OmpOutput.txt", "r") as f:
       else:
         if not (row[0] in ompResults[row[1]].keys()):
           ompResults[row[1]][row[0]] = float(row[2])        
-print(ompResults)
+
+#print(ompResults)
+
+x = []
+y = []
+
+for n, seconds in sequentialResults.items():
+    x.append(int(n))
+    y.append(float(seconds))
+
+plt.plot(x, y, label="Sequential")
+plt.xlabel("n")
+plt.ylabel("Seconds")
+plt.title("Sequential Results")
+plt.legend()
+plt.show()
+
+
+
+for n in ompResults.keys():
+  data = ompResults[n]
+  for threads_number, seconds in data.items():
+      plt.plot(int(threads_number), float(seconds), 'o', label=f"{threads_number} threads")
+
+  plt.xlabel("Threads")
+  plt.ylabel("Seconds")
+  plt.title(f"OMP Results for n={n}")
+  plt.legend()
+  plt.show()
+
 
