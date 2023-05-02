fbef54df704cb0979e5f50583dfec20b2283d891
= <=>
2023-05-01 23:06:40 -0500

    Updated

--- First Practice/matrix_mult.cu
+++ First Practice/matrix_mult.cu
@@ -118,10 +118,10 @@ int main()
     // Sequential result
     
     multiply_matrices(a,b,d,MATRIX_SIZE);
-    print_matrix(a,MATRIX_SIZE);
-    print_matrix(b,MATRIX_SIZE);
-    print_matrix(c,MATRIX_SIZE);
-    print_matrix(d,MATRIX_SIZE);
+    // print_matrix(a,MATRIX_SIZE);
+    // print_matrix(b,MATRIX_SIZE);
+    // print_matrix(c,MATRIX_SIZE);
+    // print_matrix(d,MATRIX_SIZE);
 
     bool comparison_result = compare_matrices(c,d,MATRIX_SIZE);
     
