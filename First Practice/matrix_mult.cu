384c2bb2b4e37b6af547f6b36e5b975bfeb3f1cd
= <=>
2023-05-02 14:57:37 -0500

    updated cuda

--- First Practice/matrix_mult.cu
+++ First Practice/matrix_mult.cu
@@ -140,7 +140,7 @@ int main(int argc, char *argv[])
     cudaFree(dev_b);
     cudaFree(dev_c);
     clock_gettime(CLOCK_MONOTONIC, &end);
-    elapsed_time =  (end.tv_sec - start.tv_sec)*10E6 + (end.tv_nsec - start.tv_nsec)/10E-3;
+    elapsed_time =  (end.tv_sec - start.tv_sec)*10E3 + (end.tv_nsec - start.tv_nsec)/10E-6;
     int number_of_blocks = grid_dimensions*grid_dimensions;
     printf("Matrix-size:%d - threads per block :%d - Number of blocks:%d - Time:%.3f mS", matrix_size , threads_per_block , number_of_blocks ,  elapsed_time);
     return 0;
